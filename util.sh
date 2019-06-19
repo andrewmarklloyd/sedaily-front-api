2b6760347dca3f85a6d1f1980f074d62cb081b55
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-06-18 20:32:19 -0700

    Simplify build

--- util.sh
+++ util.sh
@@ -5,9 +5,9 @@
 cd sedaily-front-end/
 npm install
 npm run build
-cd ../
-cd software-engineering-daily-api
-npm install
-npm run build
-mkdir -p dist/config/front-dist
-mv ../sedaily-front-end/dist/* dist/config/front-dist
+# cd ../
+# cd software-engineering-daily-api
+# npm install
+# npm run build
+# mkdir -p dist/config/front-dist
+# mv ../sedaily-front-end/dist/* dist/config/front-dist
