026da1142130e409029f0303cd3a4300e68bb3ca
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-06-19 09:51:33 -0700

    Update apps script

--- update.sh
+++ update.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-git submodule update --remote
+function updateApp() {
+  app=${1}
+  rm -rf ${app}
+  git clone --quiet git@github.com:SoftwareEngineeringDaily/${app}.git
+  rm -rf ${app}/.git
+  diff=$(git diff ${app})
+  if [[ -z ${diff} ]]; then
+    echo "no diff found for ${app}"
+  fi
+}
+
+updateApp sedaily-front-end
+updateApp software-engineering-daily-api
