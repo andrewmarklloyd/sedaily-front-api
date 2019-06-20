5542b89a3355190a87933c3cbb1873b787711655
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-06-19 21:52:11 -0700

    Update build

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@
 # npm run build
 # cd ../
 cd sedaily-front-end/
+ls -l node_modules/semver
 npm run build
 # mkdir -p dist/config/front-dist
 # mv ../sedaily-front-end/dist/* dist/config/front-dist
