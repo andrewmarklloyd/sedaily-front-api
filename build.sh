36e119ff19fc3ef95f863de3f0d401e88343f6e0
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-06-19 21:40:22 -0700

    update build

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@
 
 # cd sedaily-front-end/
 # npm run build
-cd ../software-engineering-daily-api
+# cd ../
+cd software-engineering-daily-api
 npm run build
 # mkdir -p dist/config/front-dist
 # mv ../sedaily-front-end/dist/* dist/config/front-dist
