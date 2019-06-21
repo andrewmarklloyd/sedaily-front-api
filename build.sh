b19e2f651587e1ce772cabdd6f6204d9b4061aff
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-06-20 20:34:09 -0700

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # requires env vars at build time?
+
 cd sedaily-front-end/
 npm run build
-# cd ../
-# cd software-engineering-daily-api
-# npm run build
-# mkdir -p dist/config/front-dist
-# mv ../sedaily-front-end/dist/* dist/config/front-dist
+
+cd ../
+cd software-engineering-daily-api
+npm run build
+
+mkdir -p dist/config/front-dist
+mv ../sedaily-front-end/dist/* dist/config/front-dist
